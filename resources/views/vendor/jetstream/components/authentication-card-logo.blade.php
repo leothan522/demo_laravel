34b391fa431aadbef90a4604c05503b27597ee54
Yonathan Castillo <leothan522@gmail.com>
2021-02-17 04:40:49 -0400

    Cambio de logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,12 +1,12 @@
 <a href="/">
-    {{--<svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
         <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
-    </svg>--}}
+    </svg>
 
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
-         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" {{--width="465px" height="463px"--}}
-         {{--viewBox="0 0 465 463"--}} viewbox="0 0 500 512" class="w-20 h-20"
+   {{-- <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" --}}{{--width="465px" height="463px"--}}{{--
+         --}}{{--viewBox="0 0 465 463"--}}{{-- viewbox="0 0 500 512" class="w-20 h-20"
          enable-background="new 0 0 465 463" xml:space="preserve">
         <image id="image0" width="465" height="463" x="0" y="0"
                                                                                                                                                                                                                                                        xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAdEAAAHPCAYAAAAMH+BwAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
@@ -1783,7 +1783,7 @@ u4vLlohs2lQW7zW4uLjEmPyi/H2wudvjMdIjGtGkRC+m3JiekvIf6MiSDi4uHRtxBehdXLZeVq5d
 e7jXsG4GhgCoqt8wjIc7d869Hio7qLi4uLQY14i6uLi4uLi0kP8Hr90GZPgvvZgAAAAldEVYdGRh
 dGU6Y3JlYXRlADIwMjEtMDItMTZUMTk6Mjc6NTUrMDM6MDATcGFvAAAAJXRFWHRkYXRlOm1vZGlm
 eQAyMDIxLTAyLTE2VDE5OjI3OjU1KzAzOjAwYi3Z0wAAAABJRU5ErkJggg==" />
-</svg>
+</svg>--}}
 
 
 </a>
