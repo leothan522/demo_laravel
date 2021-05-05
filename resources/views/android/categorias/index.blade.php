a209140e370cc0dfce918ca5a28ed5262df4d3f1
Yonathan Castillo <leothan522@gmail.com>
2021-05-05 11:32:11 -0400

    Correccion imagen en categorias android

--- resources/views/android/categorias/index.blade.php
+++ resources/views/android/categorias/index.blade.php
@@ -5,8 +5,8 @@
     <div class="row">
 
         @foreach($categorias as $categoria)
-            @if (!is_null($categoria->imagen) && file_exists('img/productos/'.$categoria->file_path.'/'.$categoria->imagen))
-                @php($imagen = asset('img/productos/'.$categoria->file_path.'/'.$categoria->imagen))
+            @if (!is_null($categoria->imagen) && file_exists('img/categorias/'.$categoria->file_path.'/'.$categoria->imagen))
+                @php($imagen = asset('img/categorias/'.$categoria->file_path.'/'.$categoria->imagen))
             @else
                 @php($imagen = asset('img/img-placeholder-320x320.png'))
             @endif
@@ -18,8 +18,8 @@
                                 <img class="img-circle elevation-2" src="{{ $imagen }}" alt="{{ $categoria->nombre }}">
                             </div>
                             <!-- /.widget-user-image -->
-                            <h3 class="widget-user-username">{{ strtoupper($categoria->nombre) }}</h3>
-                            <h5 class="widget-user-desc">{{ formatoMillares($categoria->disponibles, 0) }} Productos</h5>
+                            <h3 class="widget-user-username"><small>{{ strtoupper($categoria->nombre) }}</small></h3>
+                            <h5 class="widget-user-desc"><small>{{ formatoMillares($categoria->disponibles, 0) }} Productos</small></h5>
                         </div>
                     </div>
                 </a>
